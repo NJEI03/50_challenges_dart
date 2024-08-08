cd033bdac25ef63c7e5a30d7e1827f9b0a2cd7ed
NJEI03 <njeipierrick@gmail>
2024-08-08 15:02:41 +0100

    Challenge 4 done

---
+++ all_mutiplication_tables.dart
@@ -0,0 +1,11 @@
+void main(){
+ for(var i=1; i<=10;i++){
+  print("\n");
+  print("$i  times table");
+  for(int j=1;  j<=12;  j++){
+     int prdct;
+    prdct=j*i;
+    print("$j x $j = $prdct");
+  }
+ }
+}
