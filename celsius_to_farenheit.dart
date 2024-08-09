3b0089f652ecbadb99415cbb9f0f9068e3e6a74c
NJEI03 <njeipierrick@gmail>
2024-08-09 07:04:50 +0100

    Challenge 8 done

---
+++ celsius_to_farenheit.dart
@@ -0,0 +1,8 @@
+
+double celciusToFarenheit(double degC){
+ return (degC*9/5)+32;
+}
+void main(){
+ double degF=celciusToFarenheit(100.0);
+ print("100C is equal to $degF fahrenheit");
+}
--- sum_even_btwn10and30.dart
+++ sum_even_btwn10and30.dart
@@ -1,6 +1,7 @@
 void main(){
   var sum=0;
   for(var i=11;i<30;i++){
+    
     if(i%2==0){
      sum=(sum+i);
     }
