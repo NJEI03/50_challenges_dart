512b30b8ac0ecc3c351f1356fed78c6414901375
NJEI03 <njeipierrick@gmail>
2024-08-12 22:13:35 +0100

    Challenge 15 done

---
+++ recuursive_fibonacii.dart
@@ -0,0 +1,12 @@
+int fibonacci(int n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+void main() {
+  int n = 10; // Replace with the desired Fibonacci position
+  print('The $n-th Fibonacci number is: ${fibonacci(n)}');
+}
